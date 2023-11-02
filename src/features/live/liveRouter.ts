661d925243e6eaebed2496afe1961f8827f7981b
glen coden <info@simonmeyer.de>
2023-11-02 18:40:48 +0100

    finish auto tool server IP route

--- src/features/live/liveRouter.ts
+++ src/features/live/liveRouter.ts
@@ -388,7 +388,7 @@ export function liveRouter(app: TApp, socketServer: Promise<Server>) {
             })
         })
         .post('/auto_tool_server_ip', (req, res) => {
-            app.locals.autoToolServerIP = req.body.ip
+            app.locals.autoToolServerIP = req.body.data?.ip // reading from looney-auto-tool-server axios request
 
             res.json({
                 data: app.locals.autoToolServerIP,
